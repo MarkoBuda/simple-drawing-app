611daf905d12d846ee2bbe1d50afffac34147583
Marko Budimir <marko.budimir12@gmail.com>
2022-03-13 21:27:14 +0100

    Add dialog to change size of the brush

--- app/src/main/java/hr/ferit/markobudimir/simpledrawingapp/MainActivity.kt
+++ app/src/main/java/hr/ferit/markobudimir/simpledrawingapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package hr.ferit.markobudimir.simpledrawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
@@ -11,5 +13,35 @@ class MainActivity : AppCompatActivity() {
 
         drawingView = findViewById(R.id.drawingView)
         drawingView?.setSizeForBrush(20.toFloat())
+
+        val ibBrush: ImageButton = findViewById(R.id.ibBrush)
+        ibBrush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+    }
+
+    private fun showBrushSizeChooserDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+        val smallBtn: ImageButton = brushDialog.findViewById(R.id.ibSmallBrush)
+        smallBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val mediumBtn: ImageButton = brushDialog.findViewById(R.id.ibMediumBrush)
+        mediumBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val largeBtn: ImageButton = brushDialog.findViewById(R.id.ibLargeBrush)
+        largeBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
