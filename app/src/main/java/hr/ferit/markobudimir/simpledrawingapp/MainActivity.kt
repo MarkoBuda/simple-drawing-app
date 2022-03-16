9bdd755a4f2edd75c70ced86b17c96ca5dd10896
Marko Budimir <marko.budimir12@gmail.com>
2022-03-16 18:22:06 +0100

    Add color pallet

--- app/src/main/java/hr/ferit/markobudimir/simpledrawingapp/MainActivity.kt
+++ app/src/main/java/hr/ferit/markobudimir/simpledrawingapp/MainActivity.kt
@@ -4,15 +4,26 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
+    private var imageButtonCurrentPaint: ImageButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         drawingView = findViewById(R.id.drawingView)
         drawingView?.setSizeForBrush(20.toFloat())
+
+        val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.llPaintColors)
+        imageButtonCurrentPaint = linearLayoutPaintColors[1] as ImageButton
+        imageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_selected)
+        )
 
         val ibBrush: ImageButton = findViewById(R.id.ibBrush)
         ibBrush.setOnClickListener {
