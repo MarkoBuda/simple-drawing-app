491df253e59c23958167ac951daf50dfe78264a8
Marko Budimir <marko.budimir10@gmail.com>
2022-04-14 22:13:26 +0200

    Add select image functionality

--- app/src/main/java/hr/ferit/markobudimir/simpledrawingapp/MainActivity.kt
+++ app/src/main/java/hr/ferit/markobudimir/simpledrawingapp/MainActivity.kt
@@ -3,10 +3,13 @@ package hr.ferit.markobudimir.simpledrawingapp
 import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
@@ -18,6 +21,17 @@ import androidx.core.view.get
 class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
     private var imageButtonCurrentPaint: ImageButton? = null
+
+    val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+            if(result.resultCode == RESULT_OK && result.data != null){
+                val imageBackGround: ImageView = findViewById(R.id.ivBackground)
+
+                imageBackGround.setImageURI(result.data?.data)
+            }
+        }
+
     val requestPermission: ActivityResultLauncher<Array<String>> =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
             permissions ->
@@ -31,6 +45,10 @@ class MainActivity : AppCompatActivity() {
                         "Premission granted now you can read storage files.",
                         Toast.LENGTH_LONG
                     ).show()
+
+                    val pickIntent = Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLauncher.launch(pickIntent)
                 }else{
                     if(permissionName == Manifest.permission.READ_EXTERNAL_STORAGE){
                         Toast.makeText(
